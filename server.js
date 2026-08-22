cfb457ec9aac4cd7915e4bbf603b10d47161b529
dev-lumi <kishoreks1213@gmail.com>
2026-08-22 23:01:43 +0530

    small changes

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ import express from 'express'
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send("FullStack Practical developement is started ")
+    res.send("Task Management.")
 })
+
+app.get('/about',(req,res)=>{
+    res.send("About Page.")
+})
+app.get('/profile',(req,res)=>{
+    res.send("Profile Page.")
+})
+
 
 app.listen(3000,()=>console.log("server is running..."));
