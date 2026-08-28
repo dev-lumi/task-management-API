72d1812bab1d5c730bdb43cb1d80282e2211a49e
dev-lumi <kishoreks1213@gmail.com>
2026-08-28 22:51:44 +0530

    Tiny change

--- controllers/basicController.js
+++ controllers/basicController.js
@@ -5,7 +5,7 @@ export const basicDisplay = async(req,res)=>{
     res.send("Small change.")
 }
 export const basiCDisplay = async(req,res)=>{
-    res.send("Small change one more.")
+    res.send("Tiny change one more.")
 }
 export const aboutDisplay = async(req,res)=>{
     res.send("About.")
